e279e2a5a0ac53a9c95976bc2c14917d38be47ef
Equipo Piso 8 Sistemas <equipopiso8sistemas@javeriana.edu.co>
2024-08-23 11:26:30 -0500

    Configure ArgoCD and Jenkins

--- install-cluster.sh
+++ install-cluster.sh
@@ -4,6 +4,7 @@
 helm repo add mongodb https://mongodb.github.io/helm-charts
 helm repo add milvus https://zilliztech.github.io/milvus-helm/
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo add jenkinsci https://charts.jenkins.io
 
 # Update Repos
 helm repo update
@@ -21,4 +22,13 @@ helm upgrade --install milvusdb milvus/milvus -n milvusdb -f milvus_values.yml -
 # Install Kubernetes Dashboard
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 kubectl apply -f k8s-dashboard/
+
+# Install Jenkins
+helm upgrade --install jenkins jenkinsci/jenkins -f jenkins/jenkins-values.yaml -n jenkins --create-namespace
+kubectl apply -f jenkins/jenkins-dind-storage.yaml -n jenkins
+
+# Install
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f argocd-ingress.yaml -n argocd
 
