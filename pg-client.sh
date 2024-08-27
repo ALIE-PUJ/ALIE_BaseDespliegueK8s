a9567d02525660f7b5cab5cdf0e9ee0b3b5c5433
Equipo Piso 8 Sistemas <equipopiso8sistemas@javeriana.edu.co>
2024-08-27 12:19:30 -0500

    Feat: Adds PgClient command

---
+++ pg-client.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl run postgresdb-postgresql-ha-client --rm --tty -i --restart='Never' --namespace postgresdb --image docker.io/bitnami/postgresql-repmgr:16.4.0-debian-12-r5 --env="PGPASSWORD=$POSTGRES_PASSWORD"  \
+        --command -- psql -h postgresdb-postgresql-ha-pgpool -p 5432 -U postgres -d alie_db
